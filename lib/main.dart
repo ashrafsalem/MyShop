3f2dde06089c7487db8177949d3acac64dbe2eb6
ashrafsalem <eng.ashrafsalem@gmail.com>
2020-09-09 15:13:10 +0200

    finish starter design and structure for the myshop app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'screens/product_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.orangeAccent,
+        fontFamily: 'Lato',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductOverViewScreen(),
+    );
+  }
+}
+
