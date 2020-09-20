5a3d4732ef1761832879423904bfbd808cc82507
ashrafsalem <eng.ashrafsalem@gmail.com>
2020-09-20 16:53:28 +0200

    test

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
-class Product  with ChangeNotifier{
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -15,10 +18,29 @@ class Product  with ChangeNotifier{
     @required this.price,
     @required this.imageUrl,
     this.isFavorite = false,
-});
+  });
 
-  void toggleFavoriteStatus(){
+  void _setFavValue(bool val) {
+    isFavorite = val;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldValue = isFavorite;
+    final url = 'https://flutter-update-9681a.firebaseio.com/products/$id.json';
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+
+      if (response.statusCode >= 400) {
+        _setFavValue(oldValue);
+      }
+    } catch (error) {
+      _setFavValue(oldValue);
+    }
   }
 }
